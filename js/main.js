7b94439aecbc43df0f79c9d1eddce6065d99afe2
silvindoci <silvesterndoci@hotmailcom>
2018-11-09 17:04:52 +0100

    ddd

--- js/main.js
+++ js/main.js
@@ -2,3 +2,20 @@ $(".toggle ").click(function() {
     $(this).toggleClass('on');
     $(".toggle-menu ").toggleClass("active");
 });
+
+$(document).ready(function() {
+    $bHeight = $(".box222").height();
+    $sHeight = $('.scrollBar').height();
+    $sliderHeight = $sHeight / $bHeight * 15;
+    $('.slider-thumb').height($sliderHeight + '%');
+    $('.slider-thumb').draggable({
+        containment: 'parent',
+        axis: 'y',
+        drag: function() {
+            $pos = $('.slider-thumb').position().top;
+            $ScrollPercent = $pos / $sHeight * 100;
+            $ScrollPx = $ScrollPercent / 100 * $bHeight;
+            $('.scroll-content').scrollTop($ScrollPx);
+        }
+    })
+});
